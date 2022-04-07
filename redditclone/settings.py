47394a0a8c11d7dac9ea10bbea82fdd58ca238e2
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-07 13:36:08 +0300

    added db

--- redditclone/settings.py
+++ redditclone/settings.py
@@ -79,7 +79,9 @@ WSGI_APPLICATION = 'redditclone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'reddit',
+        'USER': 'andrew',
+        'PASSWORD': 'reddit123',
     }
 }
 
